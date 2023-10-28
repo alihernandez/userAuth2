e6885d911f0f1f663df3a5bc69e9e56d3497d784
alihernandez <ali.hernandez9two@gmail.com>
2023-10-28 16:09:06 -0600

    add user, next check for duplicate users

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -1,12 +1,21 @@
+import { connectMongoDB } from "@/lib/mongodb";
+import User from "@/models/user";
 import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
 
 export async function POST(req) {
     try {
         const {name, email, password} = await req.json();
+        const hashedPassword = await bcrypt.hash(password, 10);
+        
+        await connectMongoDB();
+        await User.create({ name, email, password: hashedPassword });
 
-        console.log("Name: ", name);
-        console.log("Email: ", email);
-        console.log("Password: ", password);
+        // console.log("Name: ", name);
+        // console.log("Email: ", email);
+        // console.log("Password: ", password);
+
+
         return NextResponse.json({message: "User registered"}, { status: 201 });
     } catch (error) {
         return NextResponse.json({message: "Error occured"}, { status: 500 });
