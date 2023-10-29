7f695efa54d046afcc6e000d0dd659839dd8675b
alihernandez <ali.hernandez9two@gmail.com>
2023-10-29 14:47:42 -0600

    basic form/upload component for pic url

--- components/UserInfo.jsx
+++ components/UserInfo.jsx
@@ -16,6 +16,11 @@ export default function UserInfo() {
         <div>
           Email: <span className="font-bold">{session?.user?.email}</span>
         </div>
+        <form>
+            <input type="text" />
+            <button>Submit File</button>
+        </form>
+        <img alt="imgPreview" ></img>
         <button onClick={() => signOut()} className="bg-red-500 text-white font-bold px-6 py-2 my-3">
           Log Out
         </button>
